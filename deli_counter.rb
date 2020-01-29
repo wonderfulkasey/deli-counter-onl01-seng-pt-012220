21804830478aaf0a4fd0650b1d42df1c52e9331d
Kasey Arnold <kaseyarnold15@gmail.com>
2020-01-29 17:29:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,11 @@
 # Write your code here.
-require 'pry'
+#require 'pry'
 
 def line(katz_deli)
   if katz_deli.empty?
     puts "The line is currently empty."
+    
   else 
-    # katz_deli = ["Logan", "Avi", "Spencer"]
     current_sentence = "The line is currently:"
     counter = 1
     katz_deli.each do |p|
