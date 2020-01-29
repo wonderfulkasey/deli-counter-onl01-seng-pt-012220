1dbabb23c2227c679bef7b600d343ea436aa0127
Kasey Arnold <kaseyarnold15@gmail.com>
2020-01-29 17:35:31 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,18 +7,25 @@ def line(katz_deli)
   else 
     current_sentence = "The line is currently:"
     counter = 1
-    katz_deli.each do |p|
-      current_sentence += " #{counter}. #{p}"
+    katz_deli.each do |person|
+      current_sentence += " #{counter}. #{person}"
       counter += 1
     end
     puts current_sentence
   end
 end
 
+
+
+
+
+
 def take_a_number(katz_deli, name)
   katz_deli << name 
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
+
+
 
 
 def now_serving(katz_deli)
